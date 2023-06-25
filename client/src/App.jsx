4ed29ceadd43cff6d1e167300d52c44d1d73a03e
atulcp2001 <atulcp2001@gmail.com>
2023-06-24 18:56:08 -0700

    commit4 - get data from MongoDB

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ const handleSubmit = async () => {
   };
 
   return (
-    <div className='h-full w-full text-center mx-auto py-10 font-bold bg-slate-800'>
+    <div className='h-full w-full text-center mx-auto py-10 font-bold bg-slate-900'>
         <h2 className='text-6xl uppercase font-semibold text-white font-oswald'>Welcome to Your Next</h2>
 
         <div className='my-20 '>
@@ -44,12 +44,12 @@ const handleSubmit = async () => {
         </div>
         {options.length >0 && (
             <div className='flex flex-col items-center text-white font-oswald font-extralight'>
-                <p className='text-2xl'>Hello <span className='font-bold text-orange-600'>{name}!</span> Your options are:</p> 
+                <p className='text-3xl'>Hello <span className='font-bold text-orange-600'>{name}!</span> Your options are:</p> 
                 <br />
                 <div className='text-left'>
                         {options.map((option, index) => {
                         return(
-                            <p key={index} className='text-orange-400'>{option}</p>
+                            <p key={index} className='text-xl text-orange-400'>{option.id} - {option.desc}</p>
                         );
                         
                     })}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,55 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const port = 3000;
+const MONGO_URI = 'mongodb+srv://atulcp2001deploy2db:zCZPzqQy5IQN0GIr@cluster0.vkyq1pm.mongodb.net/deploy2db?retryWrites=true&w=majority'
+
+// Connect to MongoDB Atlas
+const connectDB = async () => {
+
+        try {
+            const conn = await mongoose.connect(MONGO_URI);
+            console.log(`Mongo DB connected: ${conn.connection.host}`)
+        } catch (error) {
+            console.error(`Error: ${error.message}`);
+            process.exit(1);
+        }
+    };
+    
+// Define a schema for the options collection
+const optionSchema = new mongoose.Schema({
+    id: Number,
+    desc: String,
+  });
+  
+  // Define a model for the options collection
+  const Option = mongoose.model('Option', optionSchema, 'options');
+
+connectDB();
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.get('/api/options', async (req,res) => {
+    try {
+        const options = await Option.find({});
+        res.json(options);
+      } catch (error) {
+        console.error('Failed to fetch options from MongoDB', error);
+        res.status(500).json({ error: 'Failed to fetch options' });
+      }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
 const optionData = [
     '1 - Find your mission',
     '2 - Address your fears',
@@ -10,26 +58,13 @@ const optionData = [
     '5 - Create your legacy',
 ];
 
+
 const optionData1 = [
-    {option:'1 - Find your mission'},
-    {option:'2 - Address your fears'},
-    {option:'3 - Deepen your relationships'},
-    {option:'4 - Design your roadmap of possibilities'},
-    {option:'5 - Create your legacy'},
+    {id: 1, desc:'Find your mission'},
+    {id: 2, desc:'Address your fears'},
+    {id: 3, desc:'Deepen your relationships'},
+    {id: 4, desc:'Design your roadmap of possibilities'},
+    {id: 5, desc:'Create your legacy'},
 ]
 
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
-
-app.get('/api/options', (req,res) => {
-    console.log('data received from client!')
-    const options = optionData;
-    res.json(options);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// atulcp2001deploy2db: zCZPzqQy5IQN0GIr
