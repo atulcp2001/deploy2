60fea669f5770ff221bfe15850dd981a538b4230
atulcp2001 <atulcp2001@gmail.com>
2023-07-05 08:23:32 -0700

    commit 19 server.js app.get * path changes

--- server/server.js
+++ server/server.js
@@ -120,12 +120,7 @@ function sendResetPasswordEmail(email, resetToken) {
 connectDB();
 
 // Serve static files (e.g., CSS, JavaScript) from the "build" folder
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-
-// // // Catch-all route to serve the "index.html" file
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // app.use(cors({origin: 'http://localhost:3001', credentials: true}));
 app.use(cors({origin: `${clientUrl}`, credentials: true}));
@@ -444,6 +439,10 @@ app.post('/reset-password/:resetToken', async (req, res) => {
   }
 });
 
+// Catch-all route to serve the "index.html" file
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
